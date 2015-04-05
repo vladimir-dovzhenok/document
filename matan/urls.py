55c921e871c0d7760f05300a63cf29e69684152b
vladimir-dovzhenok <vladimirdovzhenok@gmail.com>
2015-04-05 17:21:57 +0300

    добавлены пустые шаблоны

--- matan/urls.py
+++ matan/urls.py
@@ -1,1 +1,7 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(
+        template_name='matan/base.html')),
+    )
